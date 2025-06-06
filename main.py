7f7924bfada514907fcbd14019750422bfe0acff
oguzyucel1 <149390134+oguzyucel1@users.noreply.github.com>
2025-06-07 02:11:56 +0300

    Update main.py
    integrated newly added script modules to the main script

--- main.py
+++ main.py
@@ -7,4 +7,12 @@ print("💾 [2/3] HTML'den notlar ayrıştırılıyor ve kaydediliyor...")
 subprocess.run(["python", "save_grades.py"], check=True)
 
 print("🔍 [3/3] Değişiklik kontrolü yapılıyor...")
-subprocess.run(["python", "check_updates.py"], check=True)
+subprocess.run(["python", "check_grades.py"], check=True)
+
+print("🔍 [4/4] Duyuru kontrolü yapılıyor...")
+subprocess.run(["python", "check_announcements.py"], check=True)
+
+print("📆 [5/5] Yeni etkinlikler kontrol ediliyor...")
+subprocess.run(["python", "check_events.py"], check=True)
+
+
