5f1f75bc09aea740aecf08aab45cc8dfc4ad5f35
oguzyucel1 <149390134+oguzyucel1@users.noreply.github.com>
2025-06-07 04:15:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import subprocess
 print("🔐 [1/3] Login ve sınav sayfası çekiliyor...")
 subprocess.run(["python", "login_script.py"], check=True)
 
-print("💾 [2/3] HTML'den notlar ayrıştırılıyor ve kaydediliyor...")
-subprocess.run(["python", "save_grades.py"], check=True)
-
 print("🔍 [3/3] Değişiklik kontrolü yapılıyor...")
 subprocess.run(["python", "check_grades.py"], check=True)
 
-print("📦 [4/4] Duyuru ve etkinlikler kaydediliyor...")
-subprocess.run(["python", "save_announcements_events.py"], check=True)
+print("💾 [2/3] HTML'den notlar ayrıştırılıyor ve kaydediliyor...")
+subprocess.run(["python", "save_grades.py"], check=True)
 
 print("🔍 [5/5] Duyuru kontrolü yapılıyor...")
 subprocess.run(["python", "check_announcements.py"], check=True)
 
 print("📆 [6/6] Yeni etkinlikler kontrol ediliyor...")
 subprocess.run(["python", "check_events.py"], check=True)
+
+print("📦 [4/4] Duyuru ve etkinlikler kaydediliyor...")
+subprocess.run(["python", "save_announcements_events.py"], check=True)
+
+
 
 
